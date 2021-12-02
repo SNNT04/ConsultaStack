5eab707189703064904c59dce5aa35ee5f805bf5
Santi <santiig04@gmail.com>
2021-12-02 23:44:53 +0100

    Get Pip

--- Main.py
+++ Main.py
@@ -1,7 +1,16 @@
 import webbrowser
+from selenium import webdriver
 
 url = "https://stackoverflow.com/search?q="
 
 texto = input("¿Que quieres buscar? ")
 
+
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+
 webbrowser.open_new_tab(url + texto)
+
+driver.find_elements_by_xpath("//*[@id='question-summary-27509']/div[2]/div[1]/h3/a")
